1a464a5ed27cc588eb698c42db8964833695a9cb
Caio Lucena <caiolucena2302@gmail.com>
2025-09-02 21:42:42 -0300

    "docs: just documents the code"

--- BasicTCPClient.java
+++ BasicTCPClient.java
@@ -11,13 +11,16 @@ public class BasicTCPClient
     {
         try
         {
+            // Connects to the server running on localhost at port 12345
             Socket client = new Socket("localhost", 12345);
-
+            
+            // Receives the date object sent by the server
             ObjectInputStream in = new ObjectInputStream(client.getInputStream());
             Date currentDate = (Date) in.readObject();
             
             JOptionPane.showMessageDialog(null, "Date from server: " + currentDate.toString());
-
+            
+            // Closes the input stream and the socket
             in.close();
             System.err.println("Connection closed");
         }
--- BasicTCPServer.java
+++ BasicTCPServer.java
@@ -8,19 +8,22 @@ public class BasicTCPServer {
     public static void main(String[] args) throws IOException {
         try
         {
+            // Creates the TCP (by default) server socket
             ServerSocket serverSocket = new ServerSocket(12345);
             System.out.println("Server is listening on port 12345");
 
             while(true)
             {
+                // Accepts and waits for a new client connection
                 Socket clientSocket = serverSocket.accept();
                 System.out.println("New client connected: " + clientSocket.getInetAddress().getHostAddress());
-
+                
+                // Sends the current date to the client by using a Stream object for output to be able to send objects
                 ObjectOutputStream out = new ObjectOutputStream(clientSocket.getOutputStream());
-                out.flush();
-                out.writeObject(new Date());
-                out.close();
-                clientSocket.close();
+                out.flush(); // Flush to ensure the stream is ready and there's no leftover data
+                out.writeObject(new Date()); // Keeps the date object in the output stream buffer
+                out.close(); // Close the output stream and send the buffered data
+                clientSocket.close(); // Close the client socket
             }
         }
         catch (IOException e)
