5324e5d41b0c9271fe60c277698893a81367e0c1
Caio Lucena <caiolucena2302@gmail.com>
2025-09-09 12:32:18 -0300

    feat: implements interective loop

--- calculatorSender.java
+++ calculatorSender.java
@@ -18,49 +18,50 @@ public class calculatorSender
             Inet4Address address = (Inet4Address) Inet4Address.getByName(args[0]);
             int port = Integer.parseInt(args[1]);
             DatagramSocket socket = new DatagramSocket(port);
-
-            byte[] message = new byte[256];
-            DatagramPacket packetReceived = new DatagramPacket(message, message.length);
-            socket.receive(packetReceived);
-
-
-            String messageString = new String(packetReceived.getData()).trim();
-            String[] parts = messageString.split(" ");
-            double result = 0;
-
-            if (parts[2].equals("+"))
-            {
-                result = Double.parseDouble(parts[0]) + Double.parseDouble(parts[1]);
-            }
-            else if (parts[2].equals("-"))
-            {
-                result = Double.parseDouble(parts[0]) - Double.parseDouble(parts[1]);
-            }
-            else if (parts[2].equals("*"))
-            {
-                result = Double.parseDouble(parts[0]) * Double.parseDouble(parts[1]);
-            }
-            else if (parts[2].equals("/"))
-            {
-                result = Double.parseDouble(parts[0]) / Double.parseDouble(parts[1]);
-            }
-            else
-            {
-                System.err.println("Invalid operation: " + parts[2]);
-                System.exit(1);
-            }
-
-            DatagramPacket sendingPacket = new DatagramPacket(
-                                            Double.toString(result).getBytes(),
-                                            Double.toString(result).getBytes().length,
-                                            packetReceived.getAddress(),
-                                            packetReceived.getPort()
-                                        );
             
-            socket.send(sendingPacket);
-
-            System.out.println("Message sent to " + args[0] + " on port " + port);
-            socket.close();
+            while (true)
+            {
+                byte[] message = new byte[256];
+                DatagramPacket packetReceived = new DatagramPacket(message, message.length);
+                socket.receive(packetReceived);
+    
+                String messageString = new String(packetReceived.getData()).trim();
+                String[] parts = messageString.split(" ");
+                double result = 0;
+    
+                if (parts[2].equals("+"))
+                {
+                    result = Double.parseDouble(parts[0]) + Double.parseDouble(parts[1]);
+                }
+                else if (parts[2].equals("-"))
+                {
+                    result = Double.parseDouble(parts[0]) - Double.parseDouble(parts[1]);
+                }
+                else if (parts[2].equals("*"))
+                {
+                    result = Double.parseDouble(parts[0]) * Double.parseDouble(parts[1]);
+                }
+                else if (parts[2].equals("/"))
+                {
+                    result = Double.parseDouble(parts[0]) / Double.parseDouble(parts[1]);
+                }
+                else
+                {
+                    System.err.println("Invalid operation: " + parts[2]);
+                    System.exit(1);
+                }
+    
+                DatagramPacket sendingPacket = new DatagramPacket(
+                                                Double.toString(result).getBytes(),
+                                                Double.toString(result).getBytes().length,
+                                                packetReceived.getAddress(),
+                                                packetReceived.getPort()
+                                            );
+                
+                socket.send(sendingPacket);
+    
+                System.out.println("Message sent to " + args[0] + " on port " + port);
+            }
         }
         catch (IOException e)
         {
