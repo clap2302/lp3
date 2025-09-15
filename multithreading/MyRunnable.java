a649244347caf9356a4c2dde90b1c966bd3dbbca
Caio Lucena <caiolucena2302@gmail.com>
2025-09-15 09:04:33 -0300

    feat: introduces multithreading first implementations

---
+++ multithreading/MyRunnable.java
@@ -0,0 +1,10 @@
+public class MyRunnable implements Runnable
+{
+    static int count = 0;
+    @Override
+    public void run()
+    {
+        String name = Thread.currentThread().getName();
+        System.out.println(name + " -> " + count++);
+    }
+}
