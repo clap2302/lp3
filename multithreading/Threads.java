45714d3d1be0960924a8a22216bf5336b50a18d1
Caio Lucena <caiolucena2302@gmail.com>
2025-09-16 10:47:47 -0300

    feat: introduces more multithreading concepts

--- multithreading/Threads.java
+++ multithreading/Threads.java
@@ -7,28 +7,28 @@ public class Threads
     {
         MyRunnable runnable = new MyRunnable();
 
-        Runnable lambdaRunnable = () ->
-        {
-            for (int i = 1; i <= 5; i++)
-            {
-                System.out.println(Thread.currentThread().getName() + " - step " + i);
-                try
-                {
-                    Thread.sleep(500);
-                } catch (InterruptedException e)
-                {
-                    e.printStackTrace();
-                }
-            }
-        };
+        // Runnable lambdaRunnable = () ->
+        // {
+        //     for (int i = 1; i <= 5; i++)
+        //     {
+        //         System.out.println(Thread.currentThread().getName() + " - step " + i);
+        //         try
+        //         {
+        //             Thread.sleep(500);
+        //         } catch (InterruptedException e)
+        //         {
+        //             e.printStackTrace();
+        //         }
+        //     }
+        // };
 
-        System.out.println("Lambda threads started");
-        Thread t1 = new Thread(lambdaRunnable);
-        Thread t2 = new Thread(lambdaRunnable);
-        Thread t3 = new Thread(lambdaRunnable);
-        t1.start();
-        t2.start();
-        t3.start();
+        // System.out.println("Lambda threads started");
+        // Thread t1 = new Thread(lambdaRunnable);
+        // Thread t2 = new Thread(lambdaRunnable);
+        // Thread t3 = new Thread(lambdaRunnable);
+        // t1.start();
+        // t2.start();
+        // t3.start();
         
 
         System.out.println("\nRunnable threads started");
